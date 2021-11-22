040cf3eaa9ee93fe59d84bfaac619cdf372f348d
fujia <fujia.site@outlook.com>
2021-11-22 20:43:31 +0800

    complete system config

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
+  },
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 12,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+      "import/no-anonymous-default-export": 0,
+      "@typescript-eslint/explicit-module-boundary-types": 0,
+      "@typescript-eslint/no-empty-function": 0,
+      "@typescript-eslint/no-explicit-any": 0,
+  },
+  "ignorePatterns": ["docs", "scripts"]
+};
---
+++ scripts/gh-pages-publish.ts
@@ -0,0 +1,26 @@
+const { cd, exec, echo, touch } = require('shelljs');
+const { readFileSync } = require('fs');
+const url = require('url');
+
+let repoUrl;
+let pkg = JSON.parse(readFileSync('package.json'));
+repoUrl = pkg.repository.url;
+
+let parsedUrl = url.parse(repoUrl);
+let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
+let ghToken = process.env.GH_TOKEN
+
+echo('Deploying docs!!!');
+cd('docs');
+touch('.fujia');
+exec('git init');
+exec('git add .');
+exec('git config user.name "fujia"');
+exec('git config user.email "fujia.site@outlook.com"');
+exec('git commit -m "docs(docs): update gh-pages"');
+exec(
+  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+)
+echo("Docs deployed!!")
+
+export {}
